01ce347faaed91e07f3acae15602a9da4b7aab56
mainstreamer <artem.kyiv@outlook.com>
2014-10-30 16:58:03 +0200

    thinking of objects

--- lesson-4/hw/index.php
+++ lesson-4/hw/index.php
@@ -2,40 +2,43 @@
 //"Класс ""Ключник"", который умеет открывать и закрывать двери, врата, окна и сундуки.
 //Некоторые из данных предметов можно закрывать на ключ, некоторые - нет.
 //На вход подается строкой тип предмета, и на выходе - результат процесса (использовал ли ключник ключ для открытия)"
-abstract class Doorman {
+abstract class Opener {
+    private $is_opened = false;
+    public function open(){}
+    public function close(){}
 
 }
-class keyKeeper extends Doorman {
-    public function __construct(string $object_type) {
-
-
+class keyKeeper {
+    public function __construct($object) {
+        if ($object instanceof Lockable){  ($object->is_locked) ? $this->unlock($object) : lock($object); }
     }
-    function open() { return " was opened ";}
-    function close() { return " was closed ";}
-    function lock() { return " was locked using a key ";}
-    function unlock() { return " was unlocked using a key ";}
+    function open($object) { $object->$is_opened = true; return " was opened ";}
+    function close($object) {$object->$is_opened = false; return " was closed ";}
+    function lock($object) { $object->$is_locked = true; return " was locked using a key ";}
+    function unlock($object) { $object->$is_locked = false; return " was unlocked using a key ";}
 }
 
 
-class door implements Lockable{
-    private $is_opened = false;
+class door extends Opener implements Lockable{
+
+      private $is_locked = false;
+
+}
+class chest extends Opener implements Lockable {
+
     private $is_locked = false;
 }
-class chest implements Lockable {
-    private $is_opened = false;
+class gates extends Opener implements Lockable{
+
     private $is_locked = false;
 }
-class gates implements Lockable{
-    private $is_opened = false;
-    private $is_locked = false;
-}
-class window {
-    private $is_opened = false;
-}
+class window extends Opener{
 
-
+}
 
 
 interface Lockable {
-  public function check_lock_state($is_opened){ if($is_opened){return " is unlocked and opened ";} elseif (!$is_opened) {} };
+  public function check_lock_status($is_opened){ if($is_opened){return " is unlocked and opened ";} elseif (!$is_opened) {} };
+  public function lock(){}
+  public function unlock(){}
 }
